0d5688c2b9826cd978c5b8e8376472a1208a9073
shitalgurale <shitalgurale@gmail.com>
2026-07-28 22:50:20 +0530

    cart

--- js/scripts.js
+++ js/scripts.js
@@ -49,3 +49,129 @@ function toggleMenu(){
         icon.classList.add("fa-bars");
     }
 }
+
+// ================= CART FUNCTIONALITY =================
+
+
+let cartRows = document.querySelectorAll("tbody tr:not(.grand-total)");
+
+
+
+cartRows.forEach(function(row){
+
+
+    let plusBtn = row.querySelector(".plus");
+    let minusBtn = row.querySelector(".minus");
+    let qty = row.querySelector(".qty");
+    let price = row.querySelector(".price");
+    let removeBtn = row.querySelector(".remove");
+
+
+    // PLUS BUTTON
+
+    plusBtn.onclick = function(){
+
+        let quantity = Number(qty.innerText);
+
+        quantity++;
+
+        qty.innerText = quantity;
+
+        updateCart();
+
+    }
+
+
+
+    // MINUS BUTTON
+
+    minusBtn.onclick = function(){
+
+        let quantity = Number(qty.innerText);
+
+
+        if(quantity > 1){
+
+            quantity--;
+
+            qty.innerText = quantity;
+
+        }
+
+
+        updateCart();
+
+    }
+
+
+
+    // REMOVE BUTTON
+
+    removeBtn.onclick = function(){
+
+        row.remove();
+
+        updateCart();
+
+    }
+
+
+});
+
+
+
+
+
+// UPDATE CART TOTAL
+
+function updateCart(){
+
+
+    let rows = document.querySelectorAll("tbody tr:not(.grand-total)");
+
+    let grandTotal = 0;
+
+
+
+    rows.forEach(function(row){
+
+
+        let quantity = Number(
+            row.querySelector(".qty").innerText
+        );
+
+
+        // Remove $ sign
+
+        let price = Number(
+            row.querySelector(".price")
+            .innerText
+            .replace("$","")
+        );
+
+
+
+        let productTotal = quantity * price;
+
+
+
+        row.querySelector(".total").innerText =
+        "$" + productTotal;
+
+
+
+        grandTotal += productTotal;
+
+
+
+    });
+
+
+
+    // Update Grand Total
+
+    document.querySelector(".grand-price").innerText =
+    "$" + grandTotal;
+
+
+}
